e6cde4aecf5dd1a561afae7bd201f08bae372801
longisrad <long.israd@gmail.com>
2026-08-08 01:15:13 +0700

    Update singbox.sh

--- trunk/user/singbox/singbox.sh
+++ trunk/user/singbox/singbox.sh
@@ -296,7 +296,11 @@ EOF
 GROUP_DIR="/tmp/sing-box/groups"
 
 fetch_all_groups() {
-    sub_list_json="$(cat /etc/storage/singbox_sub.json 2>/dev/null)"
+    if [ -s "/etc/storage/singbox_sub.json" ]; then
+        sub_list_json="$(cat /etc/storage/singbox_sub.json)"
+    else
+        sub_list_json="$(nv singbox_sub_list)"
+    fi
 	
     [ -z "$sub_list_json" ] && { log "Chưa có Subscription nào trong singbox_sub_list"; return 1; }
 
